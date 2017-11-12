a5ecf6d60907bdcaf49659805271481a7b6b3ade
cristianguzmxn <31904999+cristianguzmxn@users.noreply.github.com>
2017-11-12 00:14:23 -0400

    holaa

---
+++ js/scroll.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+$(".fa").click(function() {
+     $('html, body').animate({
+         scrollTop: $("#about").offset().top
+     }, 1500);
+ });
+});
+
+$(document).ready(function() {
+$(".fa").click(function() {
+     $('html, body').animate({
+         scrollTop: $("#about").offset().top
+     }, 1000);
+ });
+});
+
